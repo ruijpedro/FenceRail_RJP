97895af2f429a92a4d0852530fc2eea160d2fb92
ruijpedro <rjpedro@gmail.com>
2026-05-29 19:22:35 +0100

    Create MainActivity.java

---
+++ android/app/src/main/java/com/rjp/fencerail/MainActivity.java
@@ -0,0 +1,23 @@
+package com.rjp.fencerail;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
