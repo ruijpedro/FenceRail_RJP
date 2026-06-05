1ff1d8e36537dbc773a0eb6b94b2425501ac51e8
ruijpedro <rjpedro@gmail.com>
2026-06-05 20:24:12 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-export default defineConfig({ plugins: [react()], build: { outDir: 'dist' } });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/FenceRail_RJP/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
+})
