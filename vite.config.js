e914710389bbafc66c6c40fc6564fdc78534db50
ruijpedro <rjpedro@gmail.com>
2026-06-25 14:30:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Vedacoes/',
-  plugins: [react()],
+  base: './',
+
+  plugins: [
+    react()
+  ],
+
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    emptyOutDir: true
+  },
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 4173
   }
 })
